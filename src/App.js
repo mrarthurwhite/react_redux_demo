b6e2034f9df024edcee1ad50a2de5b5c43a422fd
mrarthurwhite <mister.arthur.white@gmail.com>
2021-06-12 15:42:51 -0400

    added dispatch calls

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 function Counter () {
-  const count = useSelector(state => state.count)
+  const count = useSelector(state => state.count);
+  const countUp = { type: 'COUNT_UP' };
+  const dispatch = useDispatch();
 
   return (
           <div>
