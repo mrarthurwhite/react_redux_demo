ce3b87ea153748a6c1b1a75e32c8ecc2b838128b
mrarthurwhite <mister.arthur.white@gmail.com>
2021-06-12 14:00:06 -0400

    added Provider & then wrapping App with Provider & passing it the store object

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { createStore } from 'redux';
+import { createStore } from 'redux'; // this allows us to create store
+import { Provider } from 'react-redux'; 
+// we pass the store obj to top level container Provider
 
 const rootReducer =(state, action) =>{
   return state;
@@ -10,9 +12,10 @@ const rootReducer =(state, action) =>{
 
 const store = createStore(rootReducer);
 
+// code change - added Provider to wrap around App
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+      <App />
+  </Provider> /* code change */,
   document.getElementById('root')
 );
