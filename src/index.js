6cfddfe5bf875b9b9207c83900dd450d49ac477d
mrarthurwhite <mister.arthur.white@gmail.com>
2021-06-12 13:32:26 -0400

    imported createStore, created a reducer & created an instance of the store object

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import { createStore } from 'redux';
 
+const rootReducer =(state, action) =>{
+  return state;
+}
+
+const store = createStore(rootReducer);
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
