debe0adaf1d05053e652137f6141d4139815fe9d
mrarthurwhite <mister.arthur.white@gmail.com>
2021-06-12 14:46:04 -0400

    added selector to extract data out of store

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import { useSelector } from 'react-redux';
+
+function Counter () {
+  const count = useSelector(state => state.count)
+
+  return (
+          <div>
+            <p>Count: {count}</p>
+          </div>
+        );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Demo
-      </header>
-    </div>
+    <Counter />
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,17 +5,15 @@ import App from './App';
 import { createStore } from 'redux'; // this allows us to create store
 import { Provider } from 'react-redux'; 
 // we pass the store obj to top level container Provider
-
-const rootReducer =(state, action) =>{
-  return state;
-}
+import rootReducer from './reducers/rootReducer.js';
 
 const store = createStore(rootReducer);
 
-// code change - added Provider to wrap around App
+const rootElement =   document.getElementById('root')
+// added Provider to wrap around App
 ReactDOM.render(
   <Provider store={store}>
       <App />
-  </Provider> /* code change */,
-  document.getElementById('root')
+  </Provider> ,
+  rootElement
 );
